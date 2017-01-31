d82cf0f4e4e6fe9094e5db4276218011fd7e7686
Alexander <horroshow@mail.ru>
2017-01-31 13:09:46 +0500

    rabrpc handle self state

--- index.js
+++ index.js
@@ -17,23 +17,10 @@ module.exports = class RabrpcTrailpack extends Trailpack {
 
   initialize () {
     return rabrpc.configure(this.app.config.rabrpc)
-    .then(result => {
-      this.initialized = true
-      return result
-    })
-    .catch(error => {
-      this.initialized = false
-      if (typeof error === 'string') {
-        throw new lib.Errors.RabRPCError(error)
-      }
-      else {
-        throw error
-      }
-    })
   }
 
   unload () {
-    return new Promise(resolve => resolve(this.initialized ? rabrpc.shutdown() : undefined))
+    rabrpc.shutdown()
   }
 
   constructor (app) {
